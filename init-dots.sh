a093528932a54cf8d99500f3f943bb8e12c2672d
Matthew Johnson <matthewj@netflix.com>
2015-12-15 22:23:05 -0800

    add sublimetext configs

--- init-dots.sh
+++ init-dots.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-DOTFILES_DIR=$(cd $(dirname $0); pwd |sed -e "s:$HOME/::")
+DOTFILES_DIR=$(cd "$(dirname "$0")"; pwd |sed -e "s:$HOME/::")
 
 [[ ! -d "$HOME/.dotfiles.d" ]] && mkdir "$HOME/.dotfiles.d"
 
 # vim-pathogen
 [[ ! -d ./vim/autoload ]] && mkdir ./vim/autoload
 [[ ! -f ./vim/autoload/pathogen.vim ]] && curl -LSso vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-[[ ! -d ./vim/bundle ]] && mkdir ./vim/bundle
-./vim/update_bundles
+[[ ! -d ./vim/bundle ]] && mkdir ./vim/bundle && ./vim/update_bundles
 
+# install dotfiles
 ln -nsf "$DOTFILES_DIR/bash_profile" "$HOME/.bash_profile"
 ln -nsf "$DOTFILES_DIR/vim" "$HOME/.vim"
 ln -nsf "$DOTFILES_DIR/vim/vimrc" "$HOME/.vimrc"
+ln -nsf "$HOME/$DOTFILES_DIR/Git.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Git.sublime-settings"
+ln -nsf "$HOME/$DOTFILES_DIR/Groovy.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Groovy.sublime-settings"
+ln -nsf "$HOME/$DOTFILES_DIR/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+ln -nsf "$HOME/$DOTFILES_DIR/Scala.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Scala.sublime-settings"
+ln -nsf "$HOME/$DOTFILES_DIR/SublimeLinter.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/SublimeLinter.sublime-settings"
