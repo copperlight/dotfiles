f46141da2c2dbc74468cdea2baacca1656867a8e
Matthew Johnson <matthewj@netflix.com>
2016-02-17 19:39:54 -0800

    add switch jdk function

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,14 @@ set -x JAVA7_HOME (/usr/libexec/java_home -v 1.7)
 set -x JAVA8_HOME (/usr/libexec/java_home -v 1.8)
 set -x JAVA_HOME $JAVA8_HOME
 
-#switch_jdk() {
-#if echo "$JAVA_HOME" |grep -q 1.8; then
-#export JAVA_HOME="$JAVA7_HOME"
-#else
-#export JAVA_HOME="$JAVA8_HOME"
-#fi
-#echo "JAVA_HOME=$JAVA_HOME"
-#}
+function switch_jdk
+    if echo "$JAVA_HOME" |grep -q 1.8
+        set -x JAVA_HOME $JAVA7_HOME
+    else
+        set -x JAVA_HOME $JAVA8_HOME
+    end
+    echo JAVA_HOME=$JAVA_HOME
+end
 
 # go
 set -x GOPATH $HOME/gopath
@@ -19,7 +19,7 @@ set -x GOBIN $GOPATH/bin
 # pyenv
 set -x PYENV_ROOT /usr/local/var/pyenv
 if which pyenv > /dev/null
-  pyenv init - | source
+    pyenv init - | source
 end
 
 # terminal
