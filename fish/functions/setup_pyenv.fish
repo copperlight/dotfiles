12a7ec0c1a77de6c96041d325728fb2e8b6aa115
Matthew Johnson <mjjohnson33@gmail.com>
2017-10-30 03:33:31 -0700

    fix export

--- fish/functions/setup_pyenv.fish
+++ fish/functions/setup_pyenv.fish
@@ -1,6 +1,6 @@
 function setup_pyenv
-    set -g -x CFLAGS "-O2 -I(brew --prefix openssl)/include"
+    set -g -x CFLAGS -O2 -I(brew --prefix openssl)/include
     set -g -x LDFLAGS -L(brew --prefix openssl)/lib
-    echo CFLAGS=$CFLAGS
-    echo LDFLAGS=$LDFLAGS
+    echo "CFLAGS=$CFLAGS"
+    echo "LDFLAGS=$LDFLAGS"
 end
