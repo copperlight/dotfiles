a824858625987e66a5314503c181502b48b76a4b
Matthew Johnson <mjjohnson33@gmail.com>
2017-10-30 04:00:07 -0700

    remove optimization flag

--- fish/functions/setup_pyenv.fish
+++ fish/functions/setup_pyenv.fish
@@ -1,5 +1,5 @@
 function setup_pyenv
-    set -g -x CFLAGS -O2 -I(brew --prefix openssl)/include
+    set -g -x CFLAGS -I(brew --prefix openssl)/include
     set -g -x LDFLAGS -L(brew --prefix openssl)/lib
     echo "CFLAGS=$CFLAGS"
     echo "LDFLAGS=$LDFLAGS"
