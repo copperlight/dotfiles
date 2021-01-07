cbcc24cfc99ed8d22bbef8f9ac43a003d841e7b4
Matthew Johnson <mjjohnson33@gmail.com>
2021-01-07 13:21:01 -0800

    add notes on intellij merge =

--- gitconfig.bash
+++ gitconfig.bash
@@ -20,3 +20,6 @@ git config --global "user.$ID.signingkey" "$SIGNING_KEY"
 git config --global alias.identity '! git config user.name "$(git config user.$1.name)"; git config user.email "$(git config user.$1.email)"; git config user.signingkey "$(git config user.$1.signingkey)"; :'
 git config --global alias.whoami "! git var -l | grep '^GIT_.*_IDENT'"
 
+# intellij merge tool
+# idea.sh merge <left> <right> <base> <out>
+
